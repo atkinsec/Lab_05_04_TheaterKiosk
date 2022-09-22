ff749105765bfd2d96d995bf751fe25023310883
ericatkins8 <eric.atkins@hilliardschools.org>
2022-09-22 12:43:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int age = 18;
+        System.out.println("What is your age?");
+        if (age >= 21)
+            System.out.println("You get a wristband when entering.");
+        else
+            System.out.println("you do not get a wristband");
+    }
+}
